3274324fa537f6b2a913c889dd6cac74f2ceee84
farazahmad2004 <farazlhr09@gmail.com>
2026-05-01 12:16:29 +0500

    Backend game logic 1

--- server/server.ts
+++ server/server.ts
@@ -6,6 +6,7 @@ import { Socket, Server } from "socket.io";
 import http from "http";
 import { app } from "./app.ts";
 import mongoose from "mongoose";
+import { GameState, initializeGame, handleRoll, executeMove } from "./utils/gameLogic.ts";
 const PORT = process.env.PORT || 8000;
 const MONGO_URI = process.env.MONGO_URI as string;
 const server = http.createServer(app);
@@ -34,11 +35,11 @@ interface LobbyPlayer {
 let waitingLobby: LobbyPlayer[] = [];
 const availableColors = ['red', 'blue', 'green', 'yellow'];
 
+const activeGames = new Map<string, GameState>();
 io.on("connection", (socket) => {
   console.log("USER CONNECTED:", socket.id);
   socket.on("room:join", (userData) => {
     const existingPlayerIndex = waitingLobby.findIndex(p => p.userId === userData._id);
-
     if (existingPlayerIndex !== -1) {
       waitingLobby[existingPlayerIndex].socketId = socket.id;
       io.emit("lobby:update", waitingLobby);
@@ -67,13 +68,42 @@ io.on("connection", (socket) => {
   socket.on("game:start", () => {
     if (waitingLobby.length >= 2) {
       const gameId = "game_" + Date.now();
-      
+      const gameState = initializeGame(gameId, waitingLobby);
+      activeGames.set(gameId, gameState);
       io.emit("game:start", { gameId, players: waitingLobby });
       
       waitingLobby = [];
     }
   });
 
+  socket.on("game:join_room", (gameId: string) => {
+    socket.join(gameId);
+    
+    const gameState = activeGames.get(gameId);
+    if (gameState) {
+      io.to(gameId).emit("game:update", gameState);
+    }
+  });
+  socket.on("game:roll", ({ gameId, userId }) => {
+    const gameState = activeGames.get(gameId);
+    if (!gameState) return;
+    const updatedState = handleRoll(gameState, userId);    
+    if (updatedState) {
+      activeGames.set(gameId, updatedState);
+      io.to(gameId).emit("game:update", updatedState);
+    }
+  });
+  socket.on("game:move", ({ gameId, userId, tokenId }) => {
+    const gameState = activeGames.get(gameId);
+    if (!gameState) return;
+
+    const updatedState = executeMove(gameState, userId, tokenId);
+    
+    if (updatedState) {
+      activeGames.set(gameId, updatedState);
+      io.to(gameId).emit("game:update", updatedState);
+    }
+  });
   socket.on("disconnect", () => {
     console.log("USER DISCONNECTED:", socket.id);
     waitingLobby = waitingLobby.filter(p => p.socketId !== socket.id);
